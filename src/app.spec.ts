4d216f2fb3d8e1ef298436d247226e3206040e8e
Daim-Nickel-Penny <1804449@kiit.ac.in>
2024-11-02 20:28:32 +0530

    Removed Jest

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,10 +1,10 @@
-import request from "supertest";
+// import request from "supertest";
 
-import app from "./app";
+// import app from "./app";
 
-describe("Test app.ts", () => {
-  test("Catch All Route", async () => {
-    const res = await request(app).get("/");
-    expect(res.body).toEqual({ message: "Allo! Catch-all route." });
-  });
-});
+// describe("Test app.ts", () => {
+//   test("Catch All Route", async () => {
+//     const res = await request(app).get("/");
+//     expect(res.body).toEqual({ message: "Allo! Catch-all route." });
+//   });
+// });
--- src/routes/user.spec.ts
+++ src/routes/user.spec.ts
@@ -1,5 +1,5 @@
-describe("Expect to return user object", () => {
-  test("Math test", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
+// describe("Expect to return user object", () => {
+//   test("Math test", () => {
+//     expect(2 + 2).toBe(4);
+//   });
+// });
