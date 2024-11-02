31340e219d4dd162d4afd284b4aa026a83e0e331
Daim-Nickel-Penny <1804449@kiit.ac.in>
2024-11-02 20:11:24 +0530

    Jest Scripts changed

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  collectCoverageFrom: [
+    "src/**/*.ts", // Include only TypeScript files from the src directory
+    "!dist/**/*.js", // Exclude all JavaScript files in the dist folder
+  ],
   // transform: {
   //   "^.+.tsx?$": ["ts-jest",{}],
   // },
